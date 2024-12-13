b7109651b2e64d71f5674b3576d2daaee153c358
butterfly <serenitaey39@gmail.com>
2024-12-13 16:23:26 +0900

    ダミーデータの作成

--- src/app/Models/Category.php
+++ src/app/Models/Category.php
@@ -5,9 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Contact extends Model
+class Category extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['first_name','last_name','gender','email','tel','address','building','detail'];
+    protected $fillable = [
+        'content',
+    ];
 }
--- src/database/factories/ContactFactory.php
+++ src/database/factories/ContactFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\Contact;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class ContactFactory extends Factory
@@ -12,10 +13,10 @@ class ContactFactory extends Factory
     public function definition()
     {
         return [
-            'category_id' => $this->faker->randomNumber,
+            'category_id' => Category::inRandomOrder()->first()->id,
             'first_name' => $this->faker->firstName,
             'last_name' => $this->faker->lastName,
-            'gender' => $this->faker->randomElement([0, 1, 2]),
+            'gender' => $this->faker->randomElement([1, 2, 3]),
             'email' => $this->faker->safeEmail,
             'tel' => $this->faker->phoneNumber,
             'address' => $this->faker->address,
